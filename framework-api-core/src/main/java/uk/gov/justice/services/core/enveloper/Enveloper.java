e09787ae78f1fa5911e90130fa99f59f1cd2b7fe
Muhammad Uzair Baig <muhammad.baig@hmcts.net>
2018-01-29 16:59:29 +0000

    Introduce new methods in Enveloper interface to support pojo envelope

--- framework-api-core/src/main/java/uk/gov/justice/services/core/enveloper/Enveloper.java
+++ framework-api-core/src/main/java/uk/gov/justice/services/core/enveloper/Enveloper.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.services.core.enveloper;
 
+import uk.gov.justice.services.messaging.Envelope;
 import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import java.util.function.Function;
@@ -13,7 +14,7 @@ public interface Enveloper {
      * @param envelope - the envelope containing source metadata.
      * @return a function that wraps objects into an envelope.
      */
-    Function<Object, JsonEnvelope> withMetadataFrom(final JsonEnvelope envelope);
+    Function<Object, JsonEnvelope> withMetadataFrom(final Envelope<?> envelope);
 
     /**
      * Provides a function that wraps the provided object into a new {@link JsonEnvelope} using
@@ -23,5 +24,24 @@ public interface Enveloper {
      * @param name     - name of the payload.
      * @return a function that wraps objects into an envelope.
      */
-    Function<Object, JsonEnvelope> withMetadataFrom(final JsonEnvelope envelope, final String name);
+    Function<Object, JsonEnvelope> withMetadataFrom(final Envelope<?> envelope, final String name);
+
+    /**
+     * Provides a function that wraps the provided object into a new {@link Envelope} using
+     * metadata from the given envelope.
+     *
+     * @param envelope - the envelope containing source metadata.
+     * @return a function that wraps objects into an envelope.
+     */
+    <R> Function<R, Envelope<R>> withMetadataFromPojoEnvelope(final Envelope<?> envelope);
+
+    /**
+     * Provides a function that wraps the provided object into a new {@link Envelope} using
+     * metadata from the given envelope, except the name.
+     *
+     * @param envelope - the envelope containing source metadata.
+     * @param name     - name of the payload.
+     * @return a function that wraps objects into an envelope.
+     */
+    <R> Function<R, Envelope<R>> withMetadataFromPojoEnvelope(final Envelope<?> envelope, final String name);
 }

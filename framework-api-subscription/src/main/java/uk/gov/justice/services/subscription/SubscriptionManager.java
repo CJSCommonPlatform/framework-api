534d1c94f44d9b88fa66f3a8bf248f325833ecb3
Mohamed Farouk <mohamed.farouk@hmcts.net>
2018-06-01 12:52:32 +0100

    add subscription manager on start up

--- framework-api-subscription/src/main/java/uk/gov/justice/services/subscription/SubscriptionManager.java
+++ framework-api-subscription/src/main/java/uk/gov/justice/services/subscription/SubscriptionManager.java
@@ -13,4 +13,9 @@ public interface SubscriptionManager {
      * @param jsonEnvelope the {@link JsonEnvelope} to be processed
      */
     void process(final JsonEnvelope jsonEnvelope);
+
+    /**
+     * Starts the subscription process
+     */
+    void startSubscription();
 }
